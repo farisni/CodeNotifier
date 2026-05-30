19f685e2255026a31f5908cbb5b7f3d2898173ba
faris <faris.ni@outlook.com>
2026-05-30 12:20:20 +0800

    Initial Commit

---
+++ CodeNotifier/CodeNotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeNotifierApp.swift
+//  CodeNotifier
+//
+//  Created by faris on 2026/5/30.
+//
+
+import SwiftUI
+
+@main
+struct CodeNotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
